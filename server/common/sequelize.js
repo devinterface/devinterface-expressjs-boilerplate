d484ea4e9e30e42396387708c7e097823a341899
Stefano Mancini <stefano.mancini@devinterface.com>
2017-10-16 08:36:40 +0200

    removed automatic sync

--- server/common/sequelize.js
+++ server/common/sequelize.js
@@ -2,5 +2,5 @@ import Sequelize from 'sequelize'
 const env = process.env.NODE_ENV || 'development'
 const config = require('../../config/config.js')[env]
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
-sequelize.sync()
+// sequelize.sync()
 module.exports = sequelize
