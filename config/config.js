9c5bddf28d2cfd6b4b489c3b8236e218be64177a
Stefano Mancini <stefano.mancini@devinterface.com>
2017-10-27 09:59:11 +0200

    dotenv in config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
 module.exports =
 {
   development: {
-    username: 'postgres',
-    password: 'mysecretpassword',
-    database: 'devinterface_boilerplate_sequelize',
-    host: 'localhost',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres'
   },
   test: {
